3654972dd91b48273206ea638c687c1df4b2a3fe
Paul Phönixweiß <phoenixweiss@me.com>
2017-03-16 19:45:24 +0300

    books and authors created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @books_top = Book.all.limit(2)
+    @authors_top = Author.all.limit(2)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :authors
+  resources :books
   root to: 'home#index'
   
 end
