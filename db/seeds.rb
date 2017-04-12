7811ae09e21f7849078a72edb9205f4a37e2a41a
Paul Phönixweiß <phoenixweiss@me.com>
2017-04-12 20:03:05 +0300

    basic seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+authors = ["Богдан","Антон","Мария","Дарья"]
+
+authors.each do |author|
+  Author.create(name:author)
+end
+
+books = ["Книжка", "Еще книжка", "Третья книжка", "Красная книжка"]
+
+books.each.with_index(1) do |book, index|
+  Book.create(title:book, year:(1953 + index), author_id:index)
+end
