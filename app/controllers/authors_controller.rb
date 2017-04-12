cacce8ae572d1dbe1b4354e126a0441b602d0ffd
Paul Phönixweiß <phoenixweiss@me.com>
2017-04-12 20:03:34 +0300

    authors show

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,6 +3,10 @@ class AuthorsController < ApplicationController
     @authors = Author.all
   end
 
+  def show
+    @author = Author.find(params[:id])
+  end
+
   def destroy
     @author = Author.find(params[:id])
     @author.destroy
