6f45774c60f882b7e8b7b186e4f02910cec99485
Paul Phönixweiß <phoenixweiss@me.com>
2017-04-21 20:30:55 +0300

    edit for authors and book added

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,6 +3,34 @@ class AuthorsController < ApplicationController
     @authors = Author.all
   end
 
+  def new
+    @author = Author.new
+    @books = Book.all
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to @author
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @author = Author.find(params[:id])
   end
@@ -13,4 +41,11 @@ class AuthorsController < ApplicationController
 
     redirect_to authors_path
   end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name)
+  end
+
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,35 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def new
+    @book = Book.new
+    @authors = Author.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @authors = Author.all
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @book = Book.find(params[:id])
   end
@@ -13,4 +42,11 @@ class BooksController < ApplicationController
 
     redirect_to books_path
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :year, :author_id)
+  end
+
 end
