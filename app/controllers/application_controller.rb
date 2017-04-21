a982d1d857d88634d1a63d9cf795513be0262e5a
Paul Phönixweiß <phoenixweiss@me.com>
2017-04-21 18:55:02 +0300

    http_basic_auth added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :http_authenticate
+
+  private
+
+  def http_authenticate
+    return unless params[:validate] != 'true' #&& Rails.env.staging?
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == 'admin'
+    end
+  end
+
 end
